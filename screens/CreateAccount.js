32deb8ed97870dfe13e996e24d518284d7d5cf38
Fernandes072 <joaofilho8274@gmail.com>
2023-11-24 13:38:45 -0300

    Create Account Verify

--- screens/CreateAccount.js
+++ screens/CreateAccount.js
@@ -1,6 +1,9 @@
 import { useState } from 'react';
 import { StyleSheet, View, Image, Pressable } from 'react-native';
 import { Button, Input, Text } from 'react-native-elements';
+import { Picker } from '@react-native-picker/picker';
+import DropDownPicker from 'react-native-dropdown-picker';
+import api from '../src/Services/Api';
 
 export default function CreateAccount({navigation}) {
 
@@ -8,9 +11,42 @@ export default function CreateAccount({navigation}) {
     const[email, setEmail] = useState(null);
     const[registration, setRegistration] = useState(null);
     const[course, setCourse] = useState(null);
-    const[username, setUsername] = useState(null);
     const[password, setPassword] = useState(null);
     const[confirmPassword, setConfirmPassword] = useState(null);
+
+    async function createAccountVerify(){
+        console.log(name, email, registration, course, password, confirmPassword);
+        let finded = false;
+        try {
+            const response = await api.get(`/users/${registration}`);
+            console.log(response.data);
+            finded = true;
+            if (finded) {
+                throw new Error();
+            }
+        } catch (error) {
+          console.log("Matrícula não está cadastrada!");
+        }
+
+        if (password == confirmPassword && !finded) {
+            try {
+                const response = await api.post('/users', {
+                    name: name,
+                    email: email,
+                    registration: registration,
+                    course: course,
+                    password: password
+                });
+                console.log(response.data);
+
+                    createAccount();
+            } catch (error) {
+                console.log("Erro ao criar conta!");
+            }
+        } else{
+            console.log("Senhas não conferem!");
+        }
+    }
 
     const createAccount = () => {
         navigation.reset({ 
@@ -53,11 +89,36 @@ export default function CreateAccount({navigation}) {
                 inputContainerStyle={styles.input}
                 onChangeText={value => setCourse(value)}
             />
-            <Input 
-                placeholder="Usuário" 
-                inputContainerStyle={styles.input}
-                onChangeText={value => setUsername(value)}
+            {/*<Picker style={styles.pickerComponent} 
+                selectedValue={course}
+                onValueChange={value => setCourse(value)}
+            >
+                <Picker.Item label="Selecione o curso" value="" />
+                <Picker.Item label="Ciência da Computação" value="1" />
+                <Picker.Item label="Logística" value="2" />
+                <Picker.Item label="Manutenção e Suporte em Informática" value="3" />
+                <Picker.Item label="Agronegócio" value="4" />
+
+            </Picker>*/}
+
+            <DropDownPicker
+                items={[
+                    {label: 'Selecione o curso', value: null},
+                    {label: 'Ciência da Computação', value: '1'},
+                    {label: 'Logística', value: '2'},
+                    {label: 'Manutenção e Suporte em Informática', value: '3'},
+                    {label: 'Agronegócio', value: '4'}
+                ]}
+                defaultValue={course}
+                containerStyle={{height: 40, width: '90%'}}
+                style={{backgroundColor: '#fafafa'}}
+                itemStyle={{
+                    justifyContent: 'flex-start'
+                }}
+                dropDownStyle={{backgroundColor: '#fafafa'}}
+                onChangeItem={item => setCourse(item.value)}
             />
+            
             <Input 
                 placeholder="Senha" 
                 inputContainerStyle={styles.input}
@@ -75,7 +136,7 @@ export default function CreateAccount({navigation}) {
                 titleStyle={{ color: '#457918'}}
                 buttonStyle={styles.button}
                 containerStyle={styles.containerButton}
-                onPress={() => createAccount()}
+                onPress={() => createAccountVerify()}
             />
             <Pressable>
                 <Text style={styles.create} onPress={() => back()}> Fazer login </Text>
@@ -120,5 +181,14 @@ export default function CreateAccount({navigation}) {
     create: {
         color: '#457918', 
         fontWeight: 'bold'
+    },
+    pickerComponent: {
+        height: 50,
+        width: '90%',
+        borderColor: '#000',
+        borderWidth: 1,
+        borderRadius: 4,
+        marginBottom: 10,
+        backgroundColor: '7F7F7F'
     }
     });
